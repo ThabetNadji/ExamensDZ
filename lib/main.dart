f227a4389d342cd200823ddb192e476e2653e366
Nadji Thabet <jonome2012@gmail.com>
2022-03-19 19:31:45 +0100

    local to remote

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'View/main/theme.dart';
 Future<void> main() async {
   // somethig went wrong here (test pull)
   // another test
+  // local repo to remote repo
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   SharedPreferences.getInstance().then((prefs) {
